456e8cb34c487667e949f5826254304e34b1669c
zhukoff-av <mr.zhukoff@gmail.com>
2025-02-20 08:42:07 +0100

    feat: add task 2 decorator

---
+++ Basics/tasks/02.decorator.py
@@ -0,0 +1,28 @@
+def decorator_example(func):
+    def inner(*arg):
+        print("BEFORE")
+        result = func(*arg)
+        print("after")
+        return result
+
+    return inner
+
+
+@decorator_example
+def add_element(
+        new: list[str] | str,
+        src: list = []) -> list:
+    if isinstance(new, list):
+        src.extend(new)
+    else:
+        src.append(new)
+    return src
+
+
+if __name__ == "__main__":
+    value1 = add_element("test")
+    print(value1)
+    value2 = add_element(["test2", "test3"])
+    print(value2)
+    value3 = add_element(["test4"], value2)
+    print(value3)
